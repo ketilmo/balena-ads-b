e5ddbf74dcca43ca6642ec02872a6b446f7ad43e
Aaron Shaw <shawaj@gmail.com>
2025-01-01 19:02:54 +0000

    feat: add tar1090 and graphs1090 displays

---
+++ tar1090/start.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e
+
+# Check if service has been disabled through the DISABLED_SERVICES environment variable.
+
+if [[ ",$(echo -e "${DISABLED_SERVICES}" | tr -d '[:space:]')," = *",$BALENA_SERVICE_NAME,"* ]]; then
+        echo "$BALENA_SERVICE_NAME is manually disabled. Sending request to stop the service:"
+        curl --header "Content-Type:application/json" "$BALENA_SUPERVISOR_ADDRESS/v2/applications/$BALENA_APP_ID/stop-service?apikey=$BALENA_SUPERVISOR_API_KEY" -d '{"serviceName": "'$BALENA_SERVICE_NAME'"}'
+        echo " "
+        balena-idle
+fi
+
+# Verify that all the required variables are set before starting up the application.
+
+echo "Verifying settings..."
+echo " "
+sleep 2
+
+missing_variables=false
+        
+# Begin defining all the required configuration variables.
+
+[ -z "$LAT" ] && echo "Receiver latitude is missing, will abort startup." && missing_variables=true || echo "Receiver latitude is set: $LAT"
+[ -z "$LON" ] && echo "Receiver longitude is missing, will abort startup." && missing_variables=true || echo "Receiver longitude is set: $LON"
+[ -z "$BEASTHOST" ] && echo "Receiver host is missing, will abort startup." && missing_variables=true || echo "Receiver host is set: $BEASTHOST"
+[ -z "$MLATHOST" ] && echo "Receiver port is missing, will abort startup." && missing_variables=true || echo "Receiver port is set: $MLATHOST"
+
+# End defining all the required configuration variables.
+
+echo " "
+
+if [ "$missing_variables" = true ]
+then
+        echo "Settings missing, aborting..."
+        echo " "
+        balena-idle
+fi
+
+echo "Settings verified, proceeding with startup."
+echo " "
+
+LONG=$LON
